2d5858417c52a24698b3ea79311e28f06629752f
Oluwademiladeogo <bickerstethdemilade@gmail.com>
2025-05-07 14:15:48 +0100

    chore: remove server configuration files and related code

--- server/server.ts
+++
@@ -1,9 +0,0 @@
-import app from "./app"
-import { config } from "dotenv";
-config();
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  process.env.NODE_ENV == "development"
-    ? console.log(`server running on port ${port}`)
-    : "";
-});
